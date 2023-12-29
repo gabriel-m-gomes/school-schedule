a78b856c34bf7bdc127baa4595e88be64cbc0df9
Gabriel Macedo Gomes <gabriel007.macedo@gmail.com>
2023-12-29 13:47:04 -0300

    implements utils folder with helper function map HTTP Status

---
+++ app/backend/src/utils/mapStatusHTTP.ts
@@ -0,0 +1,10 @@
+function mapStatusHTTP(status: string): number {
+    switch (status) {
+        case 'SUCCESSFUL':
+            return 201
+        case 'NOT_FOUND':
+            return 404
+        default:
+            return 500
+    }
+}
