131f447f4437d3ac1b85fbdd0f620b4a3c4e728a
Gabriel Macedo Gomes <gabriel007.macedo@gmail.com>
2024-01-01 16:58:19 -0300

    adjustment to error handling in createNote Controller

--- app/backend/src/controller/ResultController.ts
+++ app/backend/src/controller/ResultController.ts
@@ -3,23 +3,27 @@ import ResultService from "../service/ResultService";
 import mapStatusHTTP from "../utils/mapStatusHTTP";
 
 class ResultController {
-    private service = new ResultService();
+  private service = new ResultService();
 
-    async getAllResults(_req: Request, res: Response) {
-        const { status, data } = await this.service.getAllResults();
-        return res.status(mapStatusHTTP(status)).json(data);
-    };
+  async getAllResults(_req: Request, res: Response) {
+    const { status, data } = await this.service.getAllResults();
+    return res.status(mapStatusHTTP(status)).json(data);
+  };
 
-    async createNoteController(req: Request, res: Response) {
-        const { status, data} = await this.service.createNoteService(req.body);
-        return res.status(mapStatusHTTP(status)).json(data);
-    };
+  async createNoteController(req: Request, res: Response) {
+    try {
+      const { status, data} = await this.service.createNoteService(req.body);
+      return res.status(mapStatusHTTP(status)).json(data);
+    } catch (error: any ) {
+      return res.status(400).json({ message: 'Insira um bimestre válido'})
+    }
+  };
 
-    async excludeController(req: Request, res: Response) {
-        const id = Number(req.params.id);
-        const { status, data } = await this.service.excludeNoteService(id);
-        return res.status(mapStatusHTTP(status)).json(data);
-    };
+  async excludeController(req: Request, res: Response) {
+    const id = Number(req.params.id);
+    const { status, data } = await this.service.excludeNoteService(id);
+    return res.status(mapStatusHTTP(status)).json(data);
+  };
 
 };
 
