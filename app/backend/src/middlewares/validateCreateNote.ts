b7ae034c59e9978a19e011d9eb104a721adb7659
Gabriel Macedo Gomes <gabriel007.macedo@gmail.com>
2024-01-01 16:01:39 -0300

    implements validations when creating notes

---
+++ app/backend/src/middlewares/validateCreateNote.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import ResultModel from '../models/ResultsModel';
+
+class ValidateCreateNote {
+  static async validateBody(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    const { bimestre, disciplina, nota } = req.body;
+    const model = new ResultModel();
+    
+    const dbData = await model.getResults();
+    const existingNote = dbData.find(
+      (data) => data.bimestre === bimestre.toUpperCase() && data.disciplina === disciplina
+    );
+
+    if (existingNote) {
+      return res.status(400).json({ message: 'Já existe uma nota registrada para esse bimestre e disciplina' });
+    }
+
+    if (nota < 0 || nota > 10) return res.status(400).json({ message: 'Nota inválida' });
+    
+
+    next();
+  }
+}
+
+
+export default ValidateCreateNote;
+
--- app/backend/src/routes/ResultRouter.ts
+++ app/backend/src/routes/ResultRouter.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import ResultController from '../controller/ResultController';
+import ValidateCreateNote from '../middlewares/validateCreateNote';
 
 const resultController = new ResultController();
 
@@ -7,7 +8,7 @@ const router = Router();
 
 router.get('/', (req: Request, res: Response) => resultController.getAllResults(req, res));
 
-router.post('/', (req: Request, res: Response) => resultController.createNoteController(req, res));
+router.post('/', ValidateCreateNote.validateBody, (req: Request, res: Response) => resultController.createNoteController(req, res));
 
 router.delete('/:id', (req: Request, res: Response) => resultController.excludeController(req, res));
 
